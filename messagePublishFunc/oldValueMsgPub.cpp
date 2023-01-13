653cf1bc145524df6ee30d8eecf6317a4583b420
MdRaihanAhmed-coder <raihan.ahmed05.96@gmail.com>
2023-01-13 11:04:59 -0700

    bitcasting the values to int

--- messagePublishFunc/oldValueMsgPub.cpp
+++ messagePublishFunc/oldValueMsgPub.cpp
@@ -21,10 +21,7 @@
 //#include "basic_log.h"
 //#include "hello.h"
 #include "llvm/ExecutionEngine/ExecutionEngine.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/DerivedTypes.h"
 
-#define NDEBUG
 
 using namespace llvm;
  
@@ -50,7 +47,7 @@ namespace {
 char AFLCoverage::ID = 0;
 
 
-/*
+
 std::unordered_set<BasicBlock*> visited_bbs;
 
 void visitBB(BasicBlock &BB, Value* condition){
@@ -123,7 +120,7 @@ void visitBB(BasicBlock &BB, Value* condition){
                 }
         }
 }
-*/
+
 
 
 
@@ -177,9 +174,8 @@ bool AFLCoverage::runOnModule(Module &M) {
 		//	errs()<<"publish function found: "<<F.getName()<<"\n";
 		//}
 		
-		//This condition is for testbed code & got all functions needed for VGR graph.
-		if(F.getName().contains("message_arrived")){
-		//if(F.getName().contains("message_arrived") || F.getName().contains("publish") || F.getName().contains("requestOrder") || F.getName().contains("startThread") || F.getName().contains("start_thread") || F.getName().contains("run") || F.getName().contains("fsmStep") || F.getName().contains("printState") || F.getName().contains("setTarget") || F.getName().contains("moveDeliveryInAndGrip") || F.getName().contains("moveNFC") ){
+		//This condition is for testbed code
+		if(F.getName().contains("message_arrived") || F.getName().contains("publish") || F.getName().contains("requestOrder")){
 		
 		//if(F.getName().contains("message_arrived") || F.getName().contains("_ZN2ft20TxtMqttFactoryClient14publishSLD_AckENS_15TxtSldAckCode_tENS_11TxtWPType_tEil")){
 		//if(F.getName().contains("message_arrived")){
@@ -187,24 +183,22 @@ bool AFLCoverage::runOnModule(Module &M) {
 			//Function* msg = &F;
 			//errs()<<"After declaring msg\n";
 			//if(msg != nullptr && F.isDeclaration()==false){
-			//for(auto user=F.user_begin();user!=F.user_end();user++){
-			//	errs()<<"user from user_begin: "<<*user->getOperand()<<"\n";
-			//}
+
 //This for loop will get the user function		
 			for(auto ui=F.use_begin();ui!=F.use_end();ui++){
-				//errs()<<F.getName()<<" is called from this callInst: "<<*ui->getUser()<<"\n";
+				errs()<<F.getName()<<" is called from this callInst: "<<*ui->getUser()<<"\n";
 				//errs()<<"Parent: "<<ui->getUser()->getParent()<<"\n";
 				//Instruction *I = *ui->getUser();
 				if(auto I = dyn_cast<Instruction>(ui->getUser())){
-					//errs()<<"Instruction: "<<*I<<"\n";
-					//errs()<<"Instruction Parent: "<<*I->getParent()<<"\n";
+					errs()<<"Instruction: "<<*I<<"\n";
+					errs()<<"Instruction Parent: "<<*I->getParent()<<"\n";
 					auto *BB = I->getParent();
 					Function* userFunction = BB->getParent();
 					//errs()<<"User function: "<<*BB->getParent()<<"\n";
 					
-					//errs()<<"User Function: "<<userFunction->getName()<<"\n";
+					errs()<<"User Function: "<<userFunction->getName()<<"\n";
 					BasicBlock::iterator IP = BB->getFirstInsertionPt();
-					//errs()<<"first inst pointer: "<<*IP<<"\n";
+					errs()<<"first inst pointer: "<<*IP<<"\n";
 					IRBuilder<> builder(&(*IP));
 					
 					std::vector<std::string> user;
@@ -273,110 +267,21 @@ bool AFLCoverage::runOnModule(Module &M) {
 					builder.CreateCall(printfFunc, argsV, "calltmp");				
 				}
 // till now arguments		
-			// getting seg fault after getting the values, So skipping values for now	
+				
 				{
-				// using the format specifier for printing the values
 					std::string format("arg_values: ");
 					for (size_t i = 0; i < arg_values.size(); ++i) {
 						format += " %s = %lu\n";
-						//format += " %s = %f\n";
 					}
-				// creating the string from format, then converting it into vector
+
 					Value *str = builder.CreateGlobalStringPtr(format, "");
 					std::vector<Value *> argsV({str});
 
-					//for (auto &v : arg_values) {
-					//	argsV.push_back(builder.CreateGlobalStringPtr(v->getName(), ""));
-					//	errs()<<"V Type: "<<*v->getType()<<"\n";
-					//	//auto result = CastInst::Create(Instruction::SIToFP, v, FloatTyID, "", BB);
-					//	//auto res = builder.CreateBitCast(v, Type::getInt64Ty(context));
-					//// Target type is 64 bit and source type is value type
-					//	Type *SourceTy = v->getType();
-					//	Type *TargetTy = Type::getInt64Ty(context);
-					//// Getting bitwidth of both source and target to convert it into the correct bit width
-					//	unsigned TargetBitWidth = cast<IntegerType>(TargetTy)->getBitWidth();
-					//	unsigned SourceBitWidth = cast<IntegerType>(SourceTy)->getBitWidth();
-					//// Appending zeros at the end of the source bits
-					//	APInt MaxValue = APInt::getSignedMaxValue(TargetBitWidth).zext(SourceBitWidth);
-					//// Converting APint to value
-					//	ConstantInt *MaxConstant = ConstantInt::get(context, MaxValue);
-					//	errs()<<"Constant: "<<*MaxConstant << " **Value: "<<MaxConstant->getValue()<<"\n";
-					//	
-					//	//auto res = builder.CreateBitCast(v,v->getType());
-					//	//auto res = builder.CreateBitCast(MaxConstant ,v->getType());
-					//	//res = builder.CreateBitCast(res, Type::getInt64Ty(context));
-					//	//Value *Ovf = builder.CreateICmpSLT(v, MaxConstant, "Ovf");
-					//	Value *result = builder.CreateSExtOrTrunc(MaxConstant, TargetTy);
-					//	auto res = builder.CreateBitCast(result ,Type::getInt64Ty(context));
-
-					//	errs()<<"Result Type: "<<*res->getType()<<"\n";
-					//	errs()<<"**res: "<<*res<<" **v: "<< *v <<"\n";
-					//	//argsV.push_back(v);
-					//	argsV.push_back(res); //getting long values instead of usual type values to get rid of seg fault.
-					//}
-					//builder.CreateCall(printfFunc, argsV, "calltmp");
-
-			
-			// New approach to get rid of seg fault		
-					//for (auto &v : arg_values) {
-					//	argsV.push_back(builder.CreateGlobalStringPtr(v->getName(), ""));
-					//	errs()<<"V Type: "<<*v->getType()<<"\n";
-					//	//auto result = CastInst::Create(Instruction::SIToFP, v, FloatTyID, "", BB);
-					//	//auto res = builder.CreateBitCast(v, Type::getInt64Ty(context));
-					//// Target type is 64 bit and source type is value type
-					//	Type *SourceTy = v->getType();
-					//	Type *TargetTy = Type::getInt64Ty(context);
-					//// Getting bit width of both source and target to convert it into the correct bit width
-					//	//unsigned TargetBitWidth = TargetTy->getIntegerBitWidth(); 
-					//	//unsigned SourceBitWidth = cast<IntegerType>(SourceTy)->getBitWidth(); 
-					//	const DataLayout &DL = M.getDataLayout();
-					//	unsigned TargetBitWidth = DL.getTypeSizeInBits(TargetTy);
-                                        //        unsigned SourceBitWidth = DL.getTypeSizeInBits(SourceTy);
-                                        //        //unsigned TargetBitWidth = DL.getTypeSizeInBits(TargetTy).getFixedValue();
-					//	//unsigned SourceBitWidth = DL.getTypeSizeInBits(SourceTy).getFixedValue();
-					//// Getting the common bits between target and source
-					//	//Type* common_bits = builder.getIntNTy(context,SourceBitWidth);
-					//// Getting the Integer bits and converting it to value
-					//	IntegerType *IntPtrTy = builder.getIntNTy(SourceBitWidth);
-					//	//assert((*IntPtrTy)>0);
-					//	//Constant *bits = Constant::getIntegerValue( IntPtrTy, APInt(IntPtrTy->getBitWidth(),0));
-					//	//Constant *bits = Constant::getIntegerValue( IntPtrTy, APInt(DL.getTypeSizeInBits(TargetTy),ArrayRef<uint64_t>(v));
-					//	//errs()<<"IntPtrTy: "<<*IntPtrTy<<" bits: "<<*bits<<"\n";
-					//	//assert((*bits)>0);
-					//	//static_assert(std::is_integral<Constant>(), "T must be integral");
-					//	//static_assert(*bits == *(Constant::getIntegerValue( IntPtrTy, APInt(IntPtrTy->getBitWidth(),0))), "Bits should be a value");
-					//	//ConstantInt *bits = ConstantInt::get(context, common_bits);
-					//	//auto res = builder.CreateBitCast(v,v->getType());
-					//	//auto res = builder.CreateBitCast(MaxConstant ,v->getType());
-					//	//res = builder.CreateBitCast(res, Type::getInt64Ty(context));
-					//	//Value *Ovf = builder.CreateICmpSLT(v, MaxConstant, "Ovf");
-					//	
-					//	errs()<<"In CreateSExtOrTrunc\n";
-					//	//Value *result = builder.CreateSExtOrTrunc(v, TargetTy);
-					//	//auto res = builder.CreateBitCast(result ,Type::getInt64Ty(context));
-
-					//	//auto res = builder.CreateBitCast(v,Type::getInt64Ty(context));
-					//	//Value *result = builder.CreateSExtOrTrunc(res, TargetTy);
-
-					//	auto res = builder.CreateBitCast(v, SourceTy);
-                                        //        Value *result = builder.CreateSExtOrTrunc(res, TargetTy);
-
-					//	errs()<<"Result Type: "<<*res->getType()<<"\n";
-					//	errs()<<"**res: "<<*res<<" **v: "<< *v <<"\n";
-					//	//argsV.push_back(v);
-					//	argsV.push_back(result); //getting long values instead of usual type values to get rid of seg fault.
-					//}
 					for (auto &v : arg_values) {
-					        argsV.push_back(builder.CreateGlobalStringPtr(v->getName(), ""));
-					        const DataLayout &DL = M.getDataLayout();
-					        unsigned SourceBitWidth = DL.getTypeSizeInBits(v->getType());
-					        IntegerType *IntTy = builder.getIntNTy(SourceBitWidth);
-					        Value *IntResult = builder.CreateBitCast(v, IntTy);
-					        Value *Int64Result = builder.CreateSExtOrTrunc(IntResult, Type::getInt64Ty(context));
-					        argsV.push_back(Int64Result);
+						argsV.push_back(builder.CreateGlobalStringPtr(v->getName(), ""));
+						argsV.push_back(v);
 					}
-					builder.CreateCall(printfFunc, argsV, "calltmp"); 
-					
+					builder.CreateCall(printfFunc, argsV, "calltmp");
 				}
 
 				//for(auto &I:BB){
