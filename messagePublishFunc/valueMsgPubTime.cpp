55d996a8cd9ef4bb63b541bbba2006fda0ad218a
mdrahmed <raihan.ahmed05.96@gmail.com>
2023-02-01 12:32:51 -0700

    solving error 137

--- messagePublishFunc/valueMsgPubTime.cpp
+++ messagePublishFunc/valueMsgPubTime.cpp
@@ -76,14 +76,15 @@ bool CPSTracker::runOnModule(Module &M) {
 		//if(F.getName() == "_ZN2ft23action_listener_publishC1Ev" || F.getName()=="_ZN2ft20TxtMqttFactoryClient23publishStationBroadcastENSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEdS6_S6_S6_l" || F.getName()=="_ZN4mqtt12async_client7publishESt10shared_ptrIKNS_7messageEEPvRNS_16iaction_listenerE" || F.getName()=="_ZN2ft20TxtMqttFactoryClient15publishStateHBWENS_13TxtLEDSCode_tENSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEliS7_"  ||F.getName()== "_ZN2ft20TxtMqttFactoryClient19publishStateStationENSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEENS_13TxtLEDSCode_tES6_liS6_" ||F.getName()=="_ZN2ft20TxtMqttFactoryClient15publishStateDSIENS_13TxtLEDSCode_tENSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEliS7_" || F.getName()=="_ZN2ft20TxtMqttFactoryClient15publishStateMPOENS_13TxtLEDSCode_tENSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEliS7_" || F.getName()=="_ZN2ft20TxtMqttFactoryClient15publishStateSLDENS_13TxtLEDSCode_tENSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEliS7_" || F.getName()=="_ZN2ft20TxtMqttFactoryClient15publishStateVGRENS_13TxtLEDSCode_tENSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEliS7_" || F.getName()=="_ZN2ft20TxtMqttFactoryClient15publishStateDSOENS_13TxtLEDSCode_tENSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEliS7_" || F.getName()=="_ZN2ft20TxtMqttFactoryClient17publishStateOrderENS_13TxtOrderStateEl" || F.getName()=="_ZN2ft20TxtMqttFactoryClient12publishNfcDSENS_12TxtWorkpieceESt3mapINS_16TxtHistoryCode_tExSt4lessIS3_ESaISt4pairIKS3_xEEEl" || F.getName()=="_ZN2ft20TxtMqttFactoryClient13publishVGR_DoENS_14TxtVgrDoCode_tEPNS_12TxtWorkpieceEl"){
 		
 		//if(F.getName() == "_ZTv0_n12_N2ft23action_listener_publishD1Ev"){
-		if(F.getName().contains("message_arrived") || F.getName()== "_ZN2ft23action_listener_publishC1Ev" ||F.getName().contains("requestOrder") || F.getName().contains("startThread") || F.getName().contains("start_thread") || F.getName().contains("run") || F.getName().contains("fsmStep") || F.getName().contains("printState") || F.getName().contains("setTarget") || F.getName().contains("moveDeliveryInAndGrip") || F.getName().contains("moveNFC") ){
+		//if(F.getName().contains("message_arrived") || F.getName()== "_ZN2ft23action_listener_publishC1Ev" ||F.getName().contains("requestOrder") || F.getName().contains("startThread") || F.getName().contains("start_thread") || F.getName().contains("run") || F.getName().contains("fsmStep") || F.getName().contains("printState") || F.getName().contains("setTarget") || F.getName().contains("moveDeliveryInAndGrip") || F.getName().contains("moveNFC") ){
 		
 		//if(F.getName().contains("message_arrived") || F.getName().contains("publish") || F.getName().contains("requestOrder") || F.getName().contains("startThread") || F.getName().contains("start_thread") || F.getName().contains("run") || F.getName().contains("fsmStep") || F.getName().contains("printState") || F.getName().contains("setTarget")){
-		//if(F.getName().contains("message_arrived") || F.getName().contains("publish") || F.getName().contains("requestOrder") || F.getName().contains("startThread") || F.getName().contains("start_thread") || F.getName().contains("run") || F.getName().contains("fsmStep") || F.getName().contains("printState") || F.getName().contains("setTarget") || F.getName().contains("moveDeliveryInAndGrip") || F.getName().contains("moveNFC") ){
+		if(F.getName().contains("message_arrived") || F.getName().contains("publish") || F.getName().contains("requestOrder") || F.getName().contains("startThread") || F.getName().contains("start_thread") || F.getName().contains("run") || F.getName().contains("fsmStep") || F.getName().contains("printState") || F.getName().contains("setTarget") || F.getName().contains("moveDeliveryInAndGrip") || F.getName().contains("moveNFC") ){
 			if(F.getName().contains("publish"))
 				errs()<<F.getName()<<"\n";	
-		//This for loop will get the user function		
+			//This for loop will get the user function		
 			for(auto ui=F.use_begin();ui!=F.use_end();ui++){
+				errs()<<"-----------inside use_begin()---------\n";
 				if(auto I = dyn_cast<Instruction>(ui->getUser())){
 					auto *BB = I->getParent();
 					Function* userFunction = BB->getParent();
@@ -108,9 +109,11 @@ bool CPSTracker::runOnModule(Module &M) {
 					}
 					builder.CreateCall(printfFunc, argsV, "calltmp");				
 				}
+				errs()<<"------outside use_begin()-------\n";
 			}
 			
 			if(!F.isDeclaration()){
+				errs()<<"------inside isDeclaration()-------\n";
 				auto &BB = F.getEntryBlock();        
 				// starting arguments		
 				std::vector<std::string> arguments;
@@ -141,6 +144,7 @@ bool CPSTracker::runOnModule(Module &M) {
                         	errs()<<"Process start time:"<< llvm::format("%0.1f", start) << "ms\n";
 
 				{
+					errs()<<"------inside arguments-------\n";
 					//// The format string for the printf function, declared as a global literal
 					std::string format("\narguments: ");
 					std::string s;
@@ -159,12 +163,14 @@ bool CPSTracker::runOnModule(Module &M) {
 					for (auto &s : arguments) {
 						argsV.push_back(builder.CreateGlobalStringPtr(s, ""));
 					}
-					builder.CreateCall(printfFunc, argsV, "calltmp");				
+					builder.CreateCall(printfFunc, argsV, "calltmp");
+					errs()<<"------outside arguments-------\n";				
 				}
 				// till now arguments
 				// Starting to record values		
 				{
 				// using the format specifier for printing the values
+					errs()<<"------inside arg_values-------\n";
 					std::string format("arg_values: ");
 					for (size_t i = 0; i < arg_values.size(); ++i) {
 						//format += " %s = %lu\n";
@@ -185,7 +191,8 @@ bool CPSTracker::runOnModule(Module &M) {
 					        Value *Int32Result = builder.CreateSExtOrTrunc(IntResult, Type::getInt32Ty(context));
 					        argsV.push_back(Int32Result);
 					}
-					builder.CreateCall(printfFunc, argsV, "calltmp"); 
+					builder.CreateCall(printfFunc, argsV, "calltmp");
+				        errs()<<"------outside arg_values-------\n";	
 				}
 
 				//Process end time is printed here
